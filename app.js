a08ef03f28753b2a1ff75ce0d5ae55b2b97f7295
Alexandr Kravtsov <akravt92@gmail.com>
2020-11-19 17:22:30 +0300

    test5

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 const PORT = process.env.PORT||8080
